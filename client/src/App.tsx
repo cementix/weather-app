22236f76166a12b52e2a9e14f2a2bacc561b559b
cementix <bodakondrackij@gmail.com>
2024-01-23 14:36:45 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "@/components/Navbar.tsx";
+
+function App() {
+  return (
+    <div className="bg-gray-100 h-lvh">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+function Navbar({}: Props) {
+  return <div className="bg-gray-50">Navbar</div>;
+}
+
+export default Navbar;
