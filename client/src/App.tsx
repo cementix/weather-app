caf8747af0161d3c1cfcc9f35e3076e3d7b71342
cementix <bodakondrackij@gmail.com>
2024-01-24 20:30:30 +0200

    theme switcher

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar.tsx";
 
 function App() {
   return (
-    <div className="bg-gray-100 h-lvh">
+    <div className="bg-indigo-50 dark:bg-gray-900 h-lvh">
       <Navbar />
     </div>
   );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,41 @@
-import { MapPinIcon } from "@heroicons/react/16/solid";
+import { MapPinIcon, SunIcon, MoonIcon } from "@heroicons/react/16/solid";
+import { useEffect, useState } from "react";
 
 type Props = {};
 
 function Navbar({}: Props) {
+  const [isThemeDark, setThemeDark] = useState(false);
+  const toggleThemeClasses =
+    "md:size-11 xs:size-7 cursor-pointer dark:text-indigo-50";
+
+  useEffect(() => {
+    if (isThemeDark) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isThemeDark]);
+
   return (
-    <nav className="flex justify-between items-center md:mx-40 sm:mx-20 xs:mx-3 font-montserrat pt-2 h-15">
+    <nav className="flex justify-between items-center md:mx-40 sm:mx-20 xs:mx-3 pt-2 h-15">
       <div>
-        <h1 className="xs:text-2xl md:text-3xl ">
+        <h1 className="xs:text-2xl md:text-3xl dark:text-indigo-50 font-montserrat">
           WEATHER<span className="text-yellow-500">.CO</span>
         </h1>
       </div>
+      {isThemeDark ? (
+        <SunIcon
+          onClick={() => setThemeDark(!isThemeDark)}
+          className={toggleThemeClasses}
+        />
+      ) : (
+        <MoonIcon
+          onClick={() => setThemeDark(!isThemeDark)}
+          className={toggleThemeClasses}
+        />
+      )}
       <div className="flex justify-between gap-2 items-center">
-        <MapPinIcon className="md:size-11 xs:size-7 cursor-pointer" />
+        <MapPinIcon className="md:size-11 xs:size-7 cursor-pointer dark:text-indigo-50" />
         <input
           type="text"
           placeholder="Your city"
