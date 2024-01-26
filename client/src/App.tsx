ccf12dddc33a6763d29592095c388c6542f6a34a
cementix <bodakondrackij@gmail.com>
2024-01-26 16:14:28 +0200

    get user position by default

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "@/components/Navbar.tsx";
+import axios from "axios";
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState<string>("light");
+  const [weatherData, setWeatherData] = useState<number[] | null>(null);
+  const [locationData, setLocationData] = useState<object | null>(null);
 
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
@@ -13,6 +16,32 @@ function App() {
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
+
+  useEffect(() => {
+    try {
+      function findMyCoordinates() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              setLocationData({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+              });
+              console.log(locationData);
+            },
+            (err) => {
+              alert(err.message);
+            }
+          );
+        } else {
+          alert("Geolocation is not supported by your browser");
+        }
+      }
+      findMyCoordinates();
+    } catch (e) {
+      console.log(e);
+    }
+  }, [weatherData]);
 
   return (
     <div
