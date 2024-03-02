fdc3b3453a95c4d0aad5b3538491aee58e6248bf
cementix <bodakondrackij@gmail.com>
2024-03-02 11:48:38 +0200

    tooltip fix

--- client/src/components/ui/animated-tooltip.tsx
+++ client/src/components/ui/animated-tooltip.tsx
@@ -64,14 +64,14 @@ export const AnimatedTooltip = ({
                   rotate: rotate,
                   whiteSpace: "nowrap",
                 }}
-                className="absolute -top-16 -left-0 translate-x-1/2 flex text-xs  flex-col items-center justify-center rounded-md bg-black z-50 shadow-xl h-24 px-4 py-2"
+                className="absolute -top-16 -left-0 translate-x-1/2 flex text-xs  flex-col items-center justify-center rounded-md bg-primary z-50 shadow-xl h-24 px-4 py-2"
               >
                 <div className="absolute z-30 w-[40%] -bottom-px bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px " />
                 <div className="absolute w-[60%] z-30 -bottom-px bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px " />
-                <div className="font-bold text-white relative z-30 text-base">
+                <div className="font-bold text-bkg relative z-30 text-base">
                   {item.name}
                 </div>
-                <div className="text-white text-xs">{item.designation}</div>
+                <div className="text-xs text-bkg">{item.designation}</div>
               </motion.div>
             )}
           </AnimatePresence>
